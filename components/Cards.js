07e67853202b4ccd54191e39d8089a337f458917
Phil Lisak <phil.lisak@gmail.com>
2020-07-20 17:56:41 -0700

    Still can't figure out how to change articles on Tab click

--- components/Cards.js
+++ components/Cards.js
@@ -21,78 +21,75 @@
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
 
-
-
 const createCard = (object) => {
-    const card = document.createElement('div');
-    const heading = document.createElement('div');
-    const author = document.createElement('div');
-    const imgContainer = document.createElement('div');
-    const authImg = document.createElement('img');
-    const authSpan = document.createElement('span');
+  const card = document.createElement('div');
+  const heading = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const authImg = document.createElement('img');
+  const authSpan = document.createElement('span');
 
-    card.classList.add('card');
-    heading.classList.add('headline');
-    author.classList.add('author');
-    imgContainer.classList.add('img-container');
+  card.classList.add('card');
+  heading.classList.add('headline');
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
 
-    heading.textContent = object.headline;
-    authImg.src = object.authorPhoto;
-    authSpan.textContent = `By ${object.authorName}`;
+  heading.textContent = object.headline;
+  authImg.src = object.authorPhoto;
+  authSpan.textContent = `By ${object.authorName}`;
 
-    card.appendChild(heading);
-    card.appendChild(author);
-    author.appendChild(imgContainer);
-    author.appendChild(authSpan);
-    imgContainer.appendChild(authImg);
+  card.appendChild(heading);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  author.appendChild(authSpan);
+  imgContainer.appendChild(authImg);
 
+  return card;
+};
 
+const enter = document.querySelector('.cards-container');
+// Created array to identify tab content from Tabs.js in order to create event listeners and display corresponding articles
+const tabArray = axios
+  .get('https://lambda-times-backend.herokuapp.com/topics')
+  .then((response) => {
+    const jsTab = response.data.topics[0];
+    const bootTab = response.data.topics[1];
+    const techTab = response.data.topics[2];
+    const jQueryTab = response.data.topics[3];
+    const nodeTab = response.data.topics[4];
+    console.log(response);
 
-    return card;
-}
-
-const enter = document.querySelector('.cards-container')
-
-    // Created array to identify tab content from Tabs.js in order to create event listeners and display corresponding articles
-    // const tabArray = axios.get('https://lambda-times-backend.herokuapp.com/topics')
-    //     .then((response) => {
-    //         const jsTab = response.data.topics[0];
-    //         const bootTab = response.data.topics[1];
-    //         const techTab = response.data.topics[2];
-    //         const jQueryTab = response.data.topics[3];
-    //         const nodeTab = response.data.topics[4];
-    //     })            
-
-    //     jsTab.addEventListener('click', (tab) => {
-    //         axios.get('https://lambda-times-backend.herokuapp.com/articles')
-    //         .then(response => {
-    //             console.log(response)
-    //             tab.forEach(article => {
-    //                 const newCard = createCard(article)
-    //                 enter.appendChild(newCard)
-    //             })
-    //         })
-    //         .catch((error) => {
-    //             console.log('Whoops!', error)
-    //         })
-    //     })
-
-
-
-axios.get('https://lambda-times-backend.herokuapp.com/articles')
-    .then(response => {
-        console.log(response)
+    axios
+      .get('https://lambda-times-backend.herokuapp.com/articles')
+      .then((response) => {
+        console.log(response);
         // TODO: Figure out how to export event listener from Tabs.js to determine which topic is being clicked on
-        response.data.articles.javascript.forEach(article => {
-            const newCard = createCard(article)
-            enter.appendChild(newCard)
-            // Add Event Listener to log headline to console when article is clicked. 
-            newCard.addEventListener('click', (obj) => {
-                const headline = obj.data.articles.headline
-                console.log(headline)
-            })
-        })
-    })
-    .catch((error) => {
-        console.log('Whoops!', error)
-    })
+        response.data.articles.javascript.forEach((article) => {
+          const newCard = createCard(article);
+          enter.appendChild(newCard);
+        });
+        // Add Event Listener to log headline to console when article is clicked.
+        newCard.addEventListener('click', () => {
+          const headline = response.data.articles.javascript.headline;
+          console.log(headline);
+        });
+      })
+      .catch((error) => {
+        console.log('Whoops!', error);
+      });
+
+    axios
+      .get('https://lambda-times-backend.herokuapp.com/articles')
+      .then((response) => {
+        jsTab
+          .addEventListener('click', () => {
+            response.forEach((article) => {
+              const newCard = createCard(article);
+              enter.appendChild(newCard);
+            });
+          })
+          .catch((error) => {
+            console.log('Whoops!', error);
+          });
+      });
+  });
--- components/Tabs.js
+++ components/Tabs.js
@@ -11,21 +11,21 @@
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
 
 const createTab = (obj) => {
-    const tab = document.createElement('div')
+  const tab = document.createElement('div');
 
-    tab.classList.add('tab')
-    tab.textContent = obj
+  tab.classList.add('tab');
+  tab.textContent = obj;
 
-    return tab
-}
+  return tab;
+};
 
-const entry = document.querySelector('.topics')
+const entry = document.querySelector('.topics');
 
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-    .then((success) => {
-        console.log(success)
-        success.data.topics.forEach((topic) => { 
-            const newTab = createTab(topic)
-            entry.appendChild(newTab)
-        }) 
-    })
+axios
+  .get('https://lambda-times-backend.herokuapp.com/topics')
+  .then((success) => {
+    success.data.topics.forEach((topic) => {
+      const newTab = createTab(topic);
+      entry.appendChild(newTab);
+    });
+  });
